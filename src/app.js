de79d35ffa74257015790819f56dc4c410ca8bfe
r63lopez <168450472+r63lopez@users.noreply.github.com>
2024-06-04 00:15:06 +0000

    Complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extentions = [".com", ".net", ".us"];
+
+  const domains = [];
+
+  for (const p of pronouns) {
+    for (const a of adjectives) {
+      for (const n of nouns) {
+        for (const e of extentions) {
+          const d = p + a + n + e;
+          domains.push(d);
+        }
+      }
+    }
+  }
+
+  console.log(domains);
+
+  document.querySelector("h6").innerText = domains;
 };
